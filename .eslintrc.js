f8bf0514033244565b668fa7b341021b3587d948
Victor <jvleon95@gmail.com>
2020-07-17 19:18:10 -0500

    improve linterne configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,13 @@ module.exports = {
   extends: ['airbnb'],
   rules: {
     'react/jsx-filename-extension': 'off',
+    "arrow-parens": 0,
+    'no-console': 0,
   },
   "globals": {
     "window": true,
     "navigator": true,
     "fetch": true,
-  }
+    "document": true
+  },
 };
